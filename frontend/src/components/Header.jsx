d68107bf1a3fef5fe01d79b2362d1625f79bce4c
wordpress-developer01 <130246057+wordpress-developer01@users.noreply.github.com>
2025-12-31 18:56:14 +0300

    connected css to Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,23 +1,26 @@
 import assets from '../assets/assets';
+import styles from './Header.module.css';
 
 function Header() {
   return (
     <div>
-        <header>
+        <header className={styles.header}>
             <div className="logo"> 
                 <img src={assets.logo} alt="logo" />
             </div>
-            <div className="navbar">
+            <div className={styles.navbar}>
                 <nav>
+                    <ul className={styles.ul}>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">Services</a></li>
                     <li><a href="#">Our Work</a></li>
                     <li><a href="#">Contact Us</a></li>
+                    </ul>
                 </nav>
             </div>
-            <div className="buttons">
+            <div className="button">
                 <button><img src={assets.moon_icon} alt="" /></button>
-                <button>Connect <img src={assets.arrow_icon} alt="arrow" /></button>
+                <button className={styles.btn}>Connect <img src={assets.arrow_icon} alt="arrow" /></button>
             </div>
         </header>
     </div>
